c8319b1fb0543511a52c8b82747fb70ee0fd2478
trifieya <49616850+trifieya@users.noreply.github.com>
2022-10-15 04:38:33 +0100

    Update OperateurServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/operateur/OperateurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/operateur/OperateurServiceTest.java
@@ -47,7 +47,7 @@ import java.util.List;
     @Test
     @Order(4)
      void testdeleteOperateur(){
-        Operateur op = os.retrieveOperateur(30);
+        Operateur op = os.retrieveOperateur(30L);
         os.deleteOperateur(op.getIdOperateur());
         //Assertions.assertEquals(- 1,os.retrieveAllOperateurs().size());
         Assertions.assertNull(os.retrieveOperateur(op.getIdOperateur()));
