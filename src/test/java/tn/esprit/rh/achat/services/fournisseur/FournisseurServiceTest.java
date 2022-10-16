fc7e673bfee77d588c9357b8f0c383ceed52b3d7
samar-b <73402699+samar-b@users.noreply.github.com>
2022-10-16 21:16:42 +0100

    Update FournisseurServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/fournisseur/FournisseurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/fournisseur/FournisseurServiceTest.java
@@ -13,10 +13,57 @@ import java.util.List;
 public class FournisseurServiceTest {
     @Autowired
     IFournisseurService fs;
-    @Test
+    
+    
+     @Test
     @Order(1)
-    public void retrieveAll() {
-        List<Fournisseur> listFournisseurs = fs.retrieveAllFournisseurs();
-        Assertions.assertEquals(0, listFournisseurs.size());
+    public void testSaveFournisseur(){
+
+        Fournisseur f= FournisseurServiceImpl.addFournisseur(Fournisseur.builder().libelle("test fournisseur").code(1).build());
+        Assertions.assertNotNull(s);
     }
+    
+    @Test
+    @Order(2)
+    public void testRetrieveAllFournisseurs() {
+        int listFournisseurs = FournisseurServiceImpl.retrieveAllFournisseurs().size();
+        FournisseurServiceImpl.addFournisseur(Fournisseur.builder().libelle("test fournisseur").code(1).build());
+        Assertions.assertEquals(listFournisseurs+1, FournisseurServiceImpl.retrieveAllFournisseurs().size());
+    }
+    
+    @Test
+    @Order(3)
+    public void testRetrieveOneFournisseur() {
+        Fournisseur f = FournisseurServiceImpl.addFournisseur(Fournisseur.builder().libelle("test fournisseur").code(1).build());
+
+        Assertions.assertEquals(f.getIdFournisseur(),FournisseurServiceImpl.retrieveFournisseur(f.getIdFournisseur()).getIdFournisseur());
+       
+    }
+    
+     @Test
+    @Order(4)
+    public void testUpdateFournisseur() {
+        Fournisseur f = FournisseurServiceImpl.addFournisseur(Fournisseur.builder().libelle("test fournisseur").code(1).build());
+        f.setQteMin(5);
+        Assertions.assertEquals(5,FournisseurServiceImpl.updateFournisseur(f).getQteMin());
+    }
+    
+    @Test
+    @Order(5)
+    public void testDeleteFournisseur(){
+
+        Fournisseur f = FournisseurServiceImpl.addFournisseur(Fournisseur.builder().libelle("test fournisseur").code(1).build());
+        FournisseurServiceImpl.deleteFournisseur(f.getIdFournisseur());
+        Assertions.assertNull(FournisseurServiceImpl.retrieveFournisseur(f.getIdFournisseur()));
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
