42a64497d51c58fed997f16336bfc453304c29f8
trifieya <49616850+trifieya@users.noreply.github.com>
2022-10-16 01:09:40 +0100

    Update OperateurServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/operateur/OperateurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/operateur/OperateurServiceTest.java
@@ -16,22 +16,65 @@ import java.util.List;
 
     @Autowired
     IOperateurService os;
+    @Test
+    @Order(2)
+     void testretrieveAllOperateurs(){
+       int listOperateur = os.retrieveAllOperateurs().size();
+        List<Operateur> listOperateurs = os.retrieveAllOperateurs();
+        Assertions.assertEquals(listOperateur,listOperateurs.size());
+    }
+
+    @Test
+    @Order(3)
+     void testretrieveOperateur(){
+       Operateur op = os.addOperateur(Operateur.builder()
+               .nom("Hamdi")
+               .prenom("Wedi")
+               .password("root")
+               .build());
+        Assertions.assertEquals(op.getIdOperateur() , os.retrieveOperateur(op.getIdOperateur()).getIdOperateur()) ;
+    }
 
     @Test
     @Order(1)
      void testaddOperateur(){
-        Operateur op =Operateur.builder()
+        Operateur op = os.addOperateur(Operateur.builder()
                 .nom("Trifi")
                 .prenom("Eya")
                 .password("root")
-                .build();
-        os.addOperateur(op);
+                .build());
         Assertions.assertNotNull(op);
 
 
     }
 
-   
+    @Test
+    @Order(5)
+     void testdeleteOperateur(){
+       Operateur op = os.addOperateur(Operateur.builder()
+               .nom("Maria")
+               .prenom("Trifi")
+               .password("root")
+               .build());
+        os.deleteOperateur(op.getIdOperateur());
+        //Assertions.assertEquals(- 1,os.retrieveAllOperateurs().size());
+        Assertions.assertNull(os.retrieveOperateur(op.getIdOperateur()));
+
+    }
+
+    @Test
+    @Order(4)
+     void tesupdateOperateur(){
+       Operateur op = os.addOperateur(Operateur.builder()
+               .nom("Noura")
+               .prenom("Trifi")
+               .password("root")
+               .build());
+        op.setPrenom("Nounou");
+        os.updateOperateur(op);
+        Assertions.assertEquals("Nounou", os.updateOperateur(op).getPrenom());
+
+    }
 
 
 
