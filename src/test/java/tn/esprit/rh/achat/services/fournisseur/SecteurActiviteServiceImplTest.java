39a4d8004dd4359f975874b0349ad4d204c2da30
samar-b <73402699+samar-b@users.noreply.github.com>
2022-10-17 16:53:32 +0100

    Update SecteurActiviteServiceImplTest.java

--- src/test/java/tn/esprit/rh/achat/services/fournisseur/SecteurActiviteServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/fournisseur/SecteurActiviteServiceImplTest.java
@@ -1,1 +1,58 @@
 
+package tn.esprit.rh.achat.Tests;
+
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.SecteurActivite;
+import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
+import tn.esprit.rh.achat.services.ISecteurActiviteService;
+
+
+@TestMethodOrder(OrderAnnotation.class)
+@SpringBootTest
+public class SecteurActiviteServiceImplTest {
+	@Autowired
+	ISecteurActiviteService secteurService;
+	
+
+	@Test
+	@Order(1)
+	public void testRetrieveAllSecteur() {
+		List<SecteurActivite> allSActivite = secteurService.retrieveAllSecteurActivite();
+		assertEquals(0, allSActivite.size());
+	}
+
+	//@Test
+	//@Order(2)
+	//public void testAddSecteur() throws ParseException {
+	////	SecteurActivite sa = new SecteurActivite(null,"test","testtest",null);
+    // SecteurActivite savedSecteur = secteurService.addSecteurActivite(sa);
+	//	assertEquals(sa.getlibelleSecteurActivite(), savedSecteur.getlibelleSecteurActivite());
+//}
+	@Test
+	@Order(3)
+	public void testRetrieveSecteur() {
+	SecteurActivite sec = secteurService.retrieveSecteurActivite(2L);
+	assertEquals(2L, sec.getIdSecteurActivite().longValue());
+	}
+//@Test
+	//@Order(4)
+	//public void testDeleteSectuer() {
+		////secteurService.deleteSecteurActivite(10L);
+		//assertNull(secteurService.deleteSecteurActivite(10L));
+	//}
+
+}
