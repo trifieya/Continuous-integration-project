d267298e49523c47ff4b014a67502068a6d1febd
Eya <eya.trifi.19@gmail.com>
2022-10-17 18:27:41 +0100

    Mockito tests

--- src/test/java/tn/esprit/rh/achat/services/operateur/OperateurServiceMockito.java
+++ src/test/java/tn/esprit/rh/achat/services/operateur/OperateurServiceMockito.java
@@ -14,7 +14,9 @@ import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
 
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.List;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -24,15 +26,46 @@ public class OperateurServiceMockito {
     @InjectMocks
     OperateurServiceImpl operateurService;
 
+    Operateur op = Operateur.builder().nom("Eya").prenom("Trifi").password("root").build();
+    List<Operateur> listOperateurs = new ArrayList<Operateur>(){
+        {
+            add(Operateur.builder().nom("Amina").prenom("Trifi").password("root").build());
+            add(Operateur.builder().nom("Amine").prenom("Trifi").password("root").build());
+            add(Operateur.builder().nom("Noura").prenom("Trifi").password("root").build());
+            add(Operateur.builder().nom("Lassaad").prenom("Trifi").password("root").build());
 
-    Operateur op = new Operateur(22L,"Farah","Ayari","root");
+        }
+
+    };
 
     @Test
     void testretrieveOperateur(){
         Mockito.when(operateurRepositoryMock.findById(Mockito.anyLong())).thenReturn(Optional.of(op)); //find all
-        Operateur op1 = operateurService.retrieveOperateur(50L);
+        Operateur op1 = operateurService.retrieveOperateur(2L);
         Assertions.assertNotNull(op1);
 
     }
+    @Test
+    public void testaddOperateur() {
+        Mockito.when(operateurRepositoryMock.save(op)).thenReturn(op);
+        Operateur op1 = operateurService.addOperateur(op);
+        Assertions.assertNotNull(op1);
+
+    }
+
+    @Test
+    public void testretrieveAllOperateurs() {
+        Mockito.when(operateurRepositoryMock.findAll()).thenReturn(listOperateurs);
+        List<Operateur> listOp = operateurService.retrieveAllOperateurs();
+        Assertions.assertNotNull(listOp);
+    }
+
+
+
+
+
+
+
+
 
 }
