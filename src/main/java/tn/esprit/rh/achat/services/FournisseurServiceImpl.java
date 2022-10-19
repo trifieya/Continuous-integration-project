d1abc786bc413d6f6c41b7af9ecf2ab2df184397
samar-b <73402699+samar-b@users.noreply.github.com>
2022-10-19 10:09:13 +0100

    Update FournisseurServiceImpl.java

--- src/main/java/tn/esprit/rh/achat/services/FournisseurServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/FournisseurServiceImpl.java
@@ -29,7 +29,7 @@ public class FournisseurServiceImpl implements IFournisseurService {
 
 	@Override
 	public List<Fournisseur> retrieveAllFournisseurs() {
-		List<Fournisseur> fournisseurs = (List<Fournisseur>) fournisseurRepository.findAll();
+		List<Fournisseur> fournisseurs =  fournisseurRepository.findAll();
 		for (Fournisseur fournisseur : fournisseurs) {
 			log.info(" fournisseur : " + fournisseur);
 		}
@@ -68,9 +68,8 @@ public class FournisseurServiceImpl implements IFournisseurService {
 	@Override
 	public Fournisseur retrieveFournisseur(Long fournisseurId) {
 
-		Fournisseur fournisseur = fournisseurRepository.findById(fournisseurId).orElse(null);
+		return fournisseurRepository.findById(fournisseurId).orElse(null);
 
-		return fournisseur;
 	}
 
 	@Override
