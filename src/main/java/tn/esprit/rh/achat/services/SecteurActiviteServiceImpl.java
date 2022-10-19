2649ecce047c21838834080effd35f374585780e
samar-b <73402699+samar-b@users.noreply.github.com>
2022-10-19 10:12:20 +0100

    Update SecteurActiviteServiceImpl.java

--- src/main/java/tn/esprit/rh/achat/services/SecteurActiviteServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/SecteurActiviteServiceImpl.java
@@ -30,15 +30,15 @@ public class SecteurActiviteServiceImpl implements ISecteurActiviteService{
 	}
 
 	@Override
-	public SecteurActivite updateSecteurActivite(SecteurActivite sa) {
-		secteurActiviteRepository.save(sa);
-		return sa;
+	public SecteurActivite updateSecteurActivite(SecteurActivite sau) {
+		secteurActiviteRepository.save(sau);
+		return sau;
 	}
 
 	@Override
 	public SecteurActivite retrieveSecteurActivite(Long id) {
-		SecteurActivite secteurActivite = secteurActiviteRepository.findById(id).orElse(null);
-		return secteurActivite;
+		return secteurActiviteRepository.findById(id).orElse(null);
+
 	}
 
 }
